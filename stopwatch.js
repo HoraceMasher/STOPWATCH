a9f8855a307bc211ab7dfb33ec685557e71c2a5d
benjamin <benja@gmail.com>
2023-03-18 16:42:35 +0300

    first commit

--- stopwatch.js
+++ stopwatch.js
@@ -1,16 +1,16 @@
 window.onload = function () {
-    var miliseconds = 00
-    var seconds = 00; 
-    var mins = 00;
-    var hrs =00 ;
-    var hours = document.getElementById("hrs")
-    var minutes = document.getElementById("mins")
-    var secs = document.getElementById("seconds")
-    var msecs = document.getElementById("miliseconds")
-    var buttonStart = document.getElementById('startBtn');
-    var buttonStop = document.getElementById('pauseBtn');
-    var buttonReset = document.getElementById('resetBtn');
-    var Interval ;
+    let miliseconds = 00;
+    let seconds = 00; 
+    let mins = 00;
+    let hrs =00 ;
+    let hours = document.getElementById("hrs")
+    let minutes = document.getElementById("mins")
+    let secs = document.getElementById("seconds")
+    let msecs = document.getElementById("miliseconds")
+    let buttonStart = document.getElementById('startBtn');
+    let buttonStop = document.getElementById('pauseBtn');
+    let buttonReset = document.getElementById('resetBtn');
+    let Interval ;
   
     buttonStart.onclick = function() {
       
@@ -25,10 +25,10 @@ window.onload = function () {
   
     buttonReset.onclick = function() {
        clearInterval(Interval);
-      hrs = "00";
-      mins = "00";
-      seconds = "00";
-        miliseconds = "00";
+      hrs = "00:";
+      mins = "00:";
+      seconds = "00:";
+        miliseconds = "00:";
       hours.innerHTML = hrs;
       minutes.innerHTML = mins;
       secs.innerHTML = seconds;
